2314a77d345e641015e0d0f4034b233b7499c2fc
Przemek Smolinski <przemeksmolinski93@gmail.com>
2019-03-31 15:13:25 +0200

    End

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import Validator.PolishIdValidator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         PolishIdValidator polishIdValidator = new PolishIdValidator();
-        System.out.println(polishIdValidator.validate("ATM546370"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nr dowodu: ");
+        String input = scanner.next();
+        System.out.println(polishIdValidator.validate(input));
     }
 }
